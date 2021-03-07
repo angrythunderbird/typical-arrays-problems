2f1d532f36651e56503bde2ee0b95bcdeb8b6b1a
angrythunderbird <headcrab.albert@gmail.com>
2021-03-07 16:30:49 +0200

    feat: add functions for min, max and average of array calculation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (typeof array != 'undefined' && Array.isArray(array) && array.length > 0) {
+    return Math.min(...array);
+  } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (typeof array != 'undefined' && Array.isArray(array) && array.length > 0) {
+    return Math.max(...array);
+  } else return 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (typeof array != 'undefined' && Array.isArray(array) && array.length > 0) {
+    const average = array.reduce((sum, element) => sum + element);
+    return (average / array.length);
+  } else return 0;
+};
